2123fe3eb1e8f960113c1f87c7caa768464eeebc
aipot <quickpass.pro@gmail.com>
2024-04-07 13:19:55 +0900

    CPU 실행 파일 수정

--- chap04/stable-diffusion/webui-user-cpu.bat
+++ chap04/stable-diffusion/webui-user-cpu.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--use-cpu-all --skip-torch-cuda-test --precision full --no-half --api
+set COMMANDLINE_ARGS=--use-cpu all --skip-torch-cuda-test --precision full --no-half --api
 
 call webui.bat
